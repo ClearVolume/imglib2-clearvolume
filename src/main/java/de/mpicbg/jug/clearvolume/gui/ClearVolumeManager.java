2a6d14a88a484909e85ad62a5c8bfed8cc09151e
Florian Jug <jug@mpi-cbg.de>
2015-05-11 14:55:11 +0200

    Version 1.0.0

--- src/main/java/de/mpicbg/jug/clearvolume/gui/ClearVolumeManager.java
+++ src/main/java/de/mpicbg/jug/clearvolume/gui/ClearVolumeManager.java
@@ -145,7 +145,6 @@ public class ClearVolumeManager< T extends RealType< T > & NativeType< T >> impl
 					voxelSizeX,
 					voxelSizeY,
 					voxelSizeZ );
-			System.out.println( ">>>>> " + voxelSizeZ );
 			c++;
 		}
 
