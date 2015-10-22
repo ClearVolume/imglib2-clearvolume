416148e0fc83ae937278469d8d368a6860fff9c0
Loic Alain Royer <royerloic@gmail.com>
2015-10-22 12:08:16 +0200

    render layer syncing

--- src/main/java/de/mpicbg/jug/clearvolume/gui/ClearVolumeManager.java
+++ src/main/java/de/mpicbg/jug/clearvolume/gui/ClearVolumeManager.java
@@ -129,6 +129,7 @@ public class ClearVolumeManager< T extends RealType< T > & NativeType< T >> impl
 						minIntensities,
 						maxIntensities );
 
+		cv.setVolumeDataUpdateAllowed(false);
 		c = 0;
 		for ( final RandomAccessibleInterval< T > img : imagesToShow ) {
 			final int sizeX = ( int ) ( img.dimension( 0 ) );
@@ -147,6 +148,7 @@ public class ClearVolumeManager< T extends RealType< T > & NativeType< T >> impl
 					voxelSizeZ );
 			c++;
 		}
+		cv.setVolumeDataUpdateAllowed(true);
 
 		return true;
 	}
